9518963fd9a7503d3f85e96a2eec74e0672b0cef
Fedor Bashilov <fbashilov@intermedia.net>
2021-06-23 13:48:42 +0300

    fix

--- s2s.js
+++ s2s.js
@@ -59,67 +59,93 @@ function setSessionToken(accessToken){
 }
 
 ///////////////////////////////
+// Rendering functions
+///////////////////////////////
+function createZIPDownloadButton(binaryZip){
+    let blob = new Blob(binaryZip, {type : "application/zip"});
+    let button = document.getElementById("")
+    let url = window.URL.createObjectURL(blob);
+    let a = document.createElement('a');
+    a.href = url;
+    a.download = id + "." + format;
+    a.click();
+}
+///////////////////////////////
 // Call recordings functions
 ///////////////////////////////
 function getCallRecs(){
     let accessToken = getSessionToken();
     let organizationId = document.getElementById("organization-id").value;
     let unifiedUserId = document.getElementById("unified-user-id").value;
-    getCallRecsRequest(organizationId, unifiedUserId, accessToken);
+
+    getCallRecsRequest(organizationId, unifiedUserId, accessToken).then(function(response) {
+        //document.getElementById("access-token-out").innerText = response;
+    }).catch(function(error){
+        console.log("Error!!! " + error);
+    });
 }
 
 function getCallRecsRequest(organizationId, unifiedUserId, accessToken, offset = 0, count = 100){
-    let http = new XMLHttpRequest();
-    let url = `https://api.intermedia.net/voice/v2/organizations/${organizationId}/users/${unifiedUserId}/call-recordings?offset=${offset}&count=${count}`;
-    http.open('GET', url, true);
+    return new Promise(function(succeed, fail) {
+        let http = new XMLHttpRequest();
+        let url = `https://api.intermedia.net/voice/v2/organizations/${organizationId}/users/${unifiedUserId}/call-recordings?offset=${offset}&count=${count}`;
+        http.open('GET', url, true);
 
-    //Headers
-    http.setRequestHeader('Authorization', `Bearer ${accessToken}`);
+        //Headers
+        http.setRequestHeader('Authorization', `Bearer ${accessToken}`);
 
-    http.send();
+        http.send();
 
-    http.onreadystatechange = function() {//Call a function when the state changes.
-        if(http.readyState == 4) {
-            if(http.status < 400){
-                console.log(http.responseText);
-            } else{
-                console.log(`Error! ` + http.responseText);    //render message
+        http.onreadystatechange = function() {//Call a function when the state changes.
+            if(http.readyState == 4) {
+                if(http.status < 400){
+                    succeed(http.response);
+                } else{
+                    fail(new Error("Request failed: " + http.statusText));
+                }
             }
         }
-    }
+    });
 }
 function getCallRecsArchive(){
     let accessToken = getSessionToken();
     let organizationId = document.getElementById("organization-id").value;
     let unifiedUserId = document.getElementById("unified-user-id").value;
     let ids = JSON.parse(document.getElementById("call-rec-id-array").value);
-    getCallRecsArchiveRequest(organizationId, unifiedUserId, ids, accessToken);
+
+    getCallRecsArchiveRequest(organizationId, unifiedUserId, ids, accessToken).then(function(response) {
+        document.getElementById("call-recs-zip-response").innerText = response;
+    }).catch(function(error){
+        console.log("Error!!! " + error);
+    });
 }
 
 function getCallRecsArchiveRequest(organizationId, unifiedUserId, ids, accessToken, format = "zip"){
-    let http = new XMLHttpRequest();
-    let url = `https://api.intermedia.net/voice/v2/organizations/${organizationId}/users/${unifiedUserId}/call-recordings/_selected/_content?format=${format}`;
-    let dataObj = {
-        "ids": ids,
-    };
+    return new Promise(function(succeed, fail) {
+        let http = new XMLHttpRequest();
+        let url = `https://api.intermedia.net/voice/v2/organizations/${organizationId}/users/${unifiedUserId}/call-recordings/_selected/_content?format=${format}`;
+        let dataObj = {
+            "ids": ids,
+        };
 
-    http.open('POST', url, true);
+        http.open('POST', url, true);
 
-    //Headers
-    http.setRequestHeader('Content-type', 'application/json');
-    http.setRequestHeader('Authorization', `Bearer ${accessToken}`);
+        //Headers
+        http.setRequestHeader('Content-type', 'application/json');
+        http.setRequestHeader('Authorization', `Bearer ${accessToken}`);
 
-    http.send(JSON.stringify(dataObj));    
+        http.send(JSON.stringify(dataObj));    
 
-    http.onreadystatechange = function() {//Call a function when the state changes.
-        if(http.readyState == 4) {
-            if(http.status < 400){
-                console.log(http.responseText);
-            } else{
-                console.log(`Error! ` + http.responseText);    //render message
+        http.onreadystatechange = function() {//Call a function when the state changes.
+            if(http.readyState == 4) {
+                if(http.status < 400){
+                    succeed(http.response);
+                } else{
+                    fail(new Error("Request failed: " + http.statusText));
+                }
             }
         }
-    }
+    });
 }
 
 function getCallRecsContent(){
@@ -127,26 +153,33 @@ function getCallRecsContent(){
     let organizationId = document.getElementById("organization-id").value;
     let unifiedUserId = document.getElementById("unified-user-id").value;
     let callRecId = document.getElementById("call-rec-id").value;
-    getCallRecsContentRequest(organizationId, unifiedUserId, callRecId, accessToken);
+
+    getCallRecsContentRequest(organizationId, unifiedUserId, callRecId, accessToken).then(function(response) {
+        document.getElementById("call-rec-content-response").innerText = response;
+    }).catch(function(error){
+        console.log("Error!!! " + error);
+    });
 }
 
 function getCallRecsContentRequest(organizationId, unifiedUserId, callRecId, accessToken){
-    let http = new XMLHttpRequest();
-    let url = `https://api.intermedia.net/voice/v2/organizations/${organizationId}/users/${unifiedUserId}/call-recordings/${callRecId}/_content`;
-    http.open('GET', url, true);
+    return new Promise(function(succeed, fail) {
+        let http = new XMLHttpRequest();
+        let url = `https://api.intermedia.net/voice/v2/organizations/${organizationId}/users/${unifiedUserId}/call-recordings/${callRecId}/_content`;
+        http.open('GET', url, true);
 
-    //Headers
-    http.setRequestHeader('Authorization', `Bearer ${accessToken}`);
+        //Headers
+        http.setRequestHeader('Authorization', `Bearer ${accessToken}`);
 
-    http.send();
+        http.send();
 
-    http.onreadystatechange = function() {//Call a function when the state changes.
-        if(http.readyState == 4) {
-            if(http.status < 400){
-                console.log(http.responseText);
-            } else{
-                console.log(`Error! ` + http.responseText);    //render message
+        http.onreadystatechange = function() {//Call a function when the state changes.
+            if(http.readyState == 4) {
+                if(http.status < 400){
+                    succeed(http.response);
+                } else{
+                    fail(new Error("Request failed: " + http.statusText));
+                }
             }
         }
-    }
+    });
 }

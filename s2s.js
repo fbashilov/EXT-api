45414915416ea001a11e797e9c20e8547ad51092
Fedor Bashilov <fbashilov@intermedia.net>
2021-06-23 20:12:39 +0300

    fix2

--- s2s.js
+++ s2s.js
@@ -61,11 +61,12 @@ function setSessionToken(accessToken){
 ///////////////////////////////
 // Rendering functions
 ///////////////////////////////
-function createFileDownloadLinkElem(binaryCode, mimeType, extension, parentNodeId){
+function createFileDownloadLinkElem(binaryCode, mimeType, extension, fileName, parentNodeId){
     let blob = new Blob([binaryCode], {type : mimeType});
     let linkElem = document.createElement("a");
     linkElem.href = URL.createObjectURL(blob);
     linkElem.download = `${fileName}.${extension}`;
+    linkElem.textContent = `Download ${fileName}`;
     document.getElementById(parentNodeId).appendChild(linkElem);
 }
 
@@ -113,7 +114,7 @@ function getCallRecsArchive(){
     let ids = JSON.parse(document.getElementById("call-rec-id-array").value);
 
     getCallRecsArchiveRequest(organizationId, unifiedUserId, ids, accessToken).then(function(response) {
-        createFileDownloadLinkElem(response, "application/zip", "zip", "get-call-recs-archive-block");
+        createFileDownloadLinkElem(response, "application/zip", "zip", "call_recs_archive", "get-call-recs-archive-block");
     }).catch(function(error){
         console.log("Error!!! " + error);
     });
@@ -154,7 +155,7 @@ function getCallRecsContent(){
     let callRecId = document.getElementById("call-rec-id").value;
 
     getCallRecsContentRequest(organizationId, unifiedUserId, callRecId, accessToken).then(function(response) {
-        createFileDownloadLinkElem(response, "audio/mpeg", "mp3", "get-call-recs-content-block");
+        createFileDownloadLinkElem(response, "audio/mpeg", "mp3", `call_rec_${callRecId}_content`, "get-call-recs-content-block");
     }).catch(function(error){
         console.log("Error!!! " + error);
     });
